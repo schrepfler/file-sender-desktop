3219c93851eb59b70830089e1b3f305423fff32c
Jesus de Mula Cano <demula@gmail.com>
2016-01-02 13:16:37 +0100

    remove null check with option and pattern matching

--- src/main/scala-2.11/filesender/utils.scala
+++ src/main/scala-2.11/filesender/utils.scala
@@ -15,16 +15,16 @@ case class SceneContext[T](scene: Scene, controller:T)
 trait ProxyActorFactory[T] {
   val resourcePath = ""
   def sceneContext = {
-    val resource = getClass.getResource(resourcePath)
-    if (resource == null) {
-      throw new IOException("Cannot load resource:" + resource)
+    Option(getClass.getResource(resourcePath)) match {
+      case None => throw new IOException("Cannot load default resource path for class:" + getClass.getSimpleName)
+      case Some(path) =>
+        val loader = new FXMLLoader(path, new DependenciesByType(Map()))
+        loader.load()
+        val root = loader.getRoot[jfxs.Parent]()
+        val scene = new Scene(root)
+        val controller = loader.getController[T]()
+        new SceneContext[T](scene, controller)
     }
-    val loader = new FXMLLoader(resource, new DependenciesByType(Map()))
-    loader.load()
-    val root = loader.getRoot[jfxs.Parent]()
-    val scene = new Scene(root)
-    val controller = loader.getController[T]()
-    new SceneContext[T](scene, controller)
   }
 
   def actorOf(actorContext: ActorContext, uiContext: SceneContext[T]):ActorRef
